d53beae5a8cf3d86f19866e953a5e573e5ace98e
jmarantz@google.com <jmarantz@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2011-05-06 11:55:02 +0000

    Create branch 17

---
+++ branches/17/src/net/instaweb/rewriter/public/image_rewrite_filter.h
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: jmaessen@google.com (Jan Maessen)
+
+#ifndef NET_INSTAWEB_REWRITER_PUBLIC_IMAGE_REWRITE_FILTER_H_
+#define NET_INSTAWEB_REWRITER_PUBLIC_IMAGE_REWRITE_FILTER_H_
+
+#include "base/scoped_ptr.h"
+#include "net/instaweb/htmlparse/public/html_element.h"
+#include "net/instaweb/rewriter/public/image_url_encoder.h"
+#include "net/instaweb/rewriter/public/rewrite_single_resource_filter.h"
+#include "net/instaweb/util/public/basictypes.h"
+#include "net/instaweb/util/public/string.h"
+#include "net/instaweb/util/public/string_util.h"
+
+namespace net_instaweb {
+class ContentType;
+class Image;
+class ImageTagScanner;
+class OutputResource;
+class Resource;
+class RewriteDriver;
+class Statistics;
+class UrlSegmentEncoder;
+class Variable;
+class WorkBound;
+
+// Identify img tags in html and optimize them.
+// TODO(jmaessen): Big open question: how best to link pulled-in resources to
+//     rewritten urls, when in general those urls will be in a different domain.
+class ImageRewriteFilter : public RewriteSingleResourceFilter {
+ public:
+  ImageRewriteFilter(RewriteDriver* driver,
+                     StringPiece path_prefix);
+  static void Initialize(Statistics* statistics);
+  virtual void StartDocumentImpl() {}
+  virtual void StartElementImpl(HtmlElement* element) {}
+  virtual void EndElementImpl(HtmlElement* element);
+  virtual const char* Name() const { return "ImageRewrite"; }
+
+  // Can we inline resource?  If so, encode its contents into the data_url,
+  // otherwise leave data_url alone.
+  static bool CanInline(
+      int image_inline_max_bytes, const StringPiece& contents,
+      const ContentType* content_type, GoogleString* data_url);
+
+ protected:
+  // Interface to RewriteSingleResourceFilter
+  virtual RewriteResult RewriteLoadedResource(const Resource* input_resource,
+                                              OutputResource* result);
+  virtual int FilterCacheFormatVersion() const;
+  virtual bool ReuseByContentHash() const;
+  virtual const UrlSegmentEncoder* encoder() const;
+
+ private:
+  // Helper methods.
+  const ContentType* ImageToContentType(const GoogleString& origin_url,
+                                        Image* image);
+  void RewriteImageUrl(HtmlElement* element, HtmlElement::Attribute* src);
+
+  scoped_ptr<const ImageTagScanner> image_filter_;
+  scoped_ptr<WorkBound> work_bound_;
+  Variable* rewrite_count_;
+  Variable* inline_count_;
+  Variable* rewrite_saved_bytes_;
+  ImageUrlEncoder encoder_;
+
+  DISALLOW_COPY_AND_ASSIGN(ImageRewriteFilter);
+};
+
+}  // namespace net_instaweb
+
+#endif  // NET_INSTAWEB_REWRITER_PUBLIC_IMAGE_REWRITE_FILTER_H_
