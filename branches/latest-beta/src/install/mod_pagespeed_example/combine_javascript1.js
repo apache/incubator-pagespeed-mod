44c46eac7ea44ba99d4aeb5a45cbd49028b0e223
jmarantz@google.com <jmarantz@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2012-06-01 17:08:51 +0000

    upgrade latest-best to 0.10.22.4

--- branches/latest-beta/src/install/mod_pagespeed_example/combine_javascript1.js
+++
@@ -1,5 +0,0 @@
-// Appends a plaintext message to the body, followed by a new line.
-function logText(message) {
-  document.body.appendChild(document.createTextNode(message));
-  document.body.appendChild(document.createElement("br"));
-}
