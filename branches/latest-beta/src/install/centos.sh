d42c03ae12134e7e8df9e33be3f59390efd6fdfb
morlovich@google.com <morlovich@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2012-08-20 14:03:39 +0000

    Syncing latest-beta to gcc-4.7 + favicon size fixes in 22, step 1/2.

--- branches/latest-beta/src/install/centos.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -x
-exec make \
-    APACHE_CONTROL_PROGRAM=/etc/init.d/httpd \
-    APACHE_USER=apache \
-    APACHE_DOC_ROOT=/var/www/html \
-    APACHE_LOG=/var/log/httpd/error_log \
-    $*
