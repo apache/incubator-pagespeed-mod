a913f08e0b7428d20be12e8d5ae94444e7dfad50
jud@google.com <jud@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2012-11-06 20:08:06 +0000

    Delete tag to remake it.

--- tags/1.1.23.1/src/install/ubuntu.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-exec make \
-    APACHE_ROOT=/etc/apache2 \
-    APACHE_MODULES=/usr/lib/apache2/modules \
-    APACHE_CONTROL_PROGRAM=/etc/init.d/apache2 \
-    APACHE_LOG=/var/log/apache2/error.log \
-    $*
