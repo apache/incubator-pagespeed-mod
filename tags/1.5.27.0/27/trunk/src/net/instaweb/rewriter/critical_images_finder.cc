3db778c59ea52c07dbadc8e2e4d12dc2f517be59
matterbury@google.com <matterbury@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2013-04-08 19:57:19 +0000

    Get latest version to fix compilation errors on build platforms.

---
+++ tags/1.5.27.0/27/trunk/src/net/instaweb/rewriter/critical_images_finder.cc
@@ -0,0 +1,383 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: pulkitg@google.com (Pulkit Goyal)
+
+#include "net/instaweb/rewriter/public/critical_images_finder.h"
+
+#include <map>
+#include <set>
+#include <utility>
+
+#include "base/logging.h"
+#include "net/instaweb/http/public/log_record.h"
+#include "net/instaweb/rewriter/critical_images.pb.h"
+#include "net/instaweb/rewriter/public/rewrite_driver.h"
+#include "net/instaweb/rewriter/public/rewrite_options.h"
+#include "net/instaweb/rewriter/public/server_context.h"
+#include "net/instaweb/util/public/property_cache.h"
+#include "net/instaweb/util/public/proto_util.h"
+#include "net/instaweb/util/public/scoped_ptr.h"
+#include "net/instaweb/util/public/statistics.h"
+#include "net/instaweb/util/public/string_util.h"
+
+namespace net_instaweb {
+
+namespace {
+
+const char kEmptyValuePlaceholder[] = "\n";
+
+// Setup the HTML and CSS critical image sets in critical_images_info from the
+// property_value. Return true if property_value had a value, and
+// deserialization of it succeeded.
+bool PopulateCriticalImagesFromPropertyValue(
+    const PropertyValue* property_value,
+    CriticalImages* critical_images) {
+  DCHECK(property_value != NULL);
+  DCHECK(critical_images != NULL);
+  if (!property_value->has_value()) {
+    return false;
+  }
+
+  // Check if we have the placeholder string value, indicating an empty value.
+  // This will be stored when we have an empty set of critical images, since the
+  // property cache doesn't store empty values.
+  if (property_value->value() == kEmptyValuePlaceholder) {
+    critical_images->Clear();
+    return true;
+  }
+
+  ArrayInputStream input(property_value->value().data(),
+                         property_value->value().size());
+  return critical_images->ParseFromZeroCopyStream(&input);
+}
+
+// Load the value of property_value into the StringSets of critical_images_info.
+bool PopulateCriticalImagesInfoFromPropertyValue(
+    const PropertyValue* property_value,
+    CriticalImagesInfo* critical_images_info) {
+  DCHECK(property_value != NULL);
+  DCHECK(critical_images_info != NULL);
+  CriticalImages crit_images;
+  if (PopulateCriticalImagesFromPropertyValue(property_value, &crit_images)) {
+    critical_images_info->html_critical_images.clear();
+    critical_images_info->html_critical_images.insert(
+        crit_images.html_critical_images().begin(),
+        crit_images.html_critical_images().end());
+    critical_images_info->css_critical_images.clear();
+    critical_images_info->css_critical_images.insert(
+        crit_images.css_critical_images().begin(),
+        crit_images.css_critical_images().end());
+    return true;
+  }
+  return false;
+}
+
+void UpdateCriticalImagesSetInProto(
+    const StringSet& critical_images_set,
+    int max_set_size,
+    int percent_needed_for_critical,
+    protobuf::RepeatedPtrField<CriticalImages::CriticalImageSet>* set_field,
+    protobuf::RepeatedPtrField<GoogleString>* critical_images_field) {
+  DCHECK_GT(max_set_size, 0);
+
+  // If we have a max_set_size of 1 we can just directly set the
+  // critical_images_field to the new set, we don't need to store any history of
+  // responses.
+  if (max_set_size == 1) {
+    critical_images_field->Clear();
+    for (StringSet::const_iterator it = critical_images_set.begin();
+         it != critical_images_set.end(); ++it) {
+      *critical_images_field->Add() = *it;
+    }
+    return;
+  }
+
+  // Update the set field first, which contains the history of up to
+  // max_set_size critical image responses. If we already have max_set_size,
+  // drop the first response, and append the new response to the end.
+  CriticalImages::CriticalImageSet* new_set;
+  if (set_field->size() >= max_set_size) {
+    DCHECK_EQ(set_field->size(), max_set_size);
+    for (int i = 1; i < set_field->size(); ++i) {
+      set_field->SwapElements(i - 1, i);
+    }
+    new_set = set_field->Mutable(set_field->size() - 1);
+    new_set->Clear();
+  } else {
+    new_set = set_field->Add();
+  }
+
+  for (StringSet::iterator i = critical_images_set.begin();
+       i != critical_images_set.end(); ++i) {
+    new_set->add_critical_images(*i);
+  }
+
+  // Now recalculate the critical image set.
+  std::map<GoogleString, int> image_count;
+  for (int i = 0; i < set_field->size(); ++i) {
+    const CriticalImages::CriticalImageSet& set = set_field->Get(i);
+    for (int j = 0; j < set.critical_images_size(); ++j) {
+      const GoogleString& img = set.critical_images(j);
+      image_count[img]++;
+    }
+  }
+
+  int num_needed_for_critical =
+      set_field->size() * percent_needed_for_critical / 100;
+  critical_images_field->Clear();
+  for (std::map<GoogleString, int>::const_iterator it = image_count.begin();
+      it != image_count.end(); ++it) {
+    if (it->second >= num_needed_for_critical) {
+      *critical_images_field->Add() = it->first;
+    }
+  }
+}
+
+}  // namespace
+
+const char CriticalImagesFinder::kCriticalImagesPropertyName[] =
+    "critical_images";
+
+const char CriticalImagesFinder::kCriticalImagesValidCount[] =
+    "critical_images_valid_count";
+
+const char CriticalImagesFinder::kCriticalImagesExpiredCount[] =
+    "critical_images_expired_count";
+
+const char CriticalImagesFinder::kCriticalImagesNotFoundCount[] =
+    "critical_images_not_found_count";
+
+CriticalImagesFinder::CriticalImagesFinder(Statistics* statistics) {
+  critical_images_valid_count_ = statistics->GetVariable(
+      kCriticalImagesValidCount);
+  critical_images_expired_count_ = statistics->GetVariable(
+      kCriticalImagesExpiredCount);
+  critical_images_not_found_count_ = statistics->GetVariable(
+      kCriticalImagesNotFoundCount);
+}
+
+CriticalImagesFinder::~CriticalImagesFinder() {
+}
+
+void CriticalImagesFinder::InitStats(Statistics* statistics) {
+  statistics->AddVariable(kCriticalImagesValidCount);
+  statistics->AddVariable(kCriticalImagesExpiredCount);
+  statistics->AddVariable(kCriticalImagesNotFoundCount);
+}
+
+bool CriticalImagesFinder::IsHtmlCriticalImage(
+    const GoogleString& image_url, RewriteDriver* driver) {
+  const StringSet& critical_images_set = GetHtmlCriticalImages(driver);
+  return (critical_images_set.find(image_url) != critical_images_set.end());
+}
+
+bool CriticalImagesFinder::IsCssCriticalImage(
+    const GoogleString& image_url, RewriteDriver* driver) {
+  const StringSet& critical_images_set = GetCssCriticalImages(driver);
+  return (critical_images_set.find(image_url) != critical_images_set.end());
+}
+
+const StringSet& CriticalImagesFinder::GetHtmlCriticalImages(
+    RewriteDriver* driver) {
+  UpdateCriticalImagesSetInDriver(driver);
+  const CriticalImagesInfo* info = driver->critical_images_info();
+  CHECK(info != NULL);
+
+  return info->html_critical_images;
+}
+
+const StringSet& CriticalImagesFinder::GetCssCriticalImages(
+    RewriteDriver* driver) {
+  UpdateCriticalImagesSetInDriver(driver);
+  const CriticalImagesInfo* info = driver->critical_images_info();
+  CHECK(info != NULL);
+
+  return info->css_critical_images;
+}
+
+StringSet* CriticalImagesFinder::mutable_html_critical_images(
+    RewriteDriver* driver) {
+  DCHECK(driver != NULL);
+  CriticalImagesInfo* driver_info = driver->critical_images_info();
+  // Preserve CSS critical images if they have been updated already.
+  if (driver_info == NULL) {
+    driver_info = new CriticalImagesInfo;
+    driver->set_critical_images_info(driver_info);
+  }
+  return &driver_info->html_critical_images;
+}
+
+StringSet* CriticalImagesFinder::mutable_css_critical_images(
+    RewriteDriver* driver) {
+  DCHECK(driver != NULL);
+  CriticalImagesInfo* driver_info = driver->critical_images_info();
+  // Preserve CSS critical images if they have been updated already.
+  if (driver_info == NULL) {
+    driver_info = new CriticalImagesInfo;
+    driver->set_critical_images_info(driver_info);
+  }
+  return &driver_info->css_critical_images;
+}
+
+
+// Copy the critical images for this request from the property cache into the
+// RewriteDriver. The critical images are not stored in CriticalImageFinder
+// because the ServerContext holds the CriticalImageFinder and hence is shared
+// between requests.
+void CriticalImagesFinder::UpdateCriticalImagesSetInDriver(
+    RewriteDriver* driver) {
+  const CriticalImagesInfo* driver_info = driver->critical_images_info();
+  // If driver_info is not NULL, then the CriticalImagesInfo has already been
+  // updated, so no need to do anything here.
+  if (driver_info != NULL) {
+    return;
+  }
+  scoped_ptr<CriticalImagesInfo> info(new CriticalImagesInfo);
+  PropertyCache* page_property_cache =
+      driver->server_context()->page_property_cache();
+  const PropertyCache::Cohort* cohort =
+      page_property_cache->GetCohort(GetCriticalImagesCohort());
+  PropertyPage* page = driver->property_page();
+  if (page != NULL && cohort != NULL) {
+    PropertyValue* property_value = page->GetProperty(
+        cohort, kCriticalImagesPropertyName);
+    ExtractCriticalImagesFromCache(driver, property_value, true, info.get());
+    driver->log_record()->SetNumHtmlCriticalImages(
+        info->html_critical_images.size());
+    driver->log_record()->SetNumCssCriticalImages(
+        info->css_critical_images.size());
+  }
+  driver->set_critical_images_info(info.release());
+}
+
+// TODO(pulkitg): Change all instances of critical_images_set to
+// html_critical_images_set.
+bool CriticalImagesFinder::UpdateCriticalImagesCacheEntryFromDriver(
+    RewriteDriver* driver, StringSet* critical_images_set,
+    StringSet* css_critical_images_set) {
+  // Update property cache if above the fold critical images are successfully
+  // determined.
+  PropertyPage* page = driver->property_page();
+  PropertyCache* page_property_cache =
+      driver->server_context()->page_property_cache();
+  return UpdateCriticalImagesCacheEntry(
+      page, page_property_cache, critical_images_set, css_critical_images_set);
+}
+
+bool CriticalImagesFinder::UpdateCriticalImagesCacheEntry(
+    PropertyPage* page, PropertyCache* page_property_cache,
+    StringSet* html_critical_images_set, StringSet* css_critical_images_set) {
+  // Update property cache if above the fold critical images are successfully
+  // determined.
+  scoped_ptr<StringSet> html_critical_images(html_critical_images_set);
+  scoped_ptr<StringSet> css_critical_images(css_critical_images_set);
+  if (page_property_cache != NULL && page != NULL) {
+    const PropertyCache::Cohort* cohort =
+        page_property_cache->GetCohort(GetCriticalImagesCohort());
+    if (cohort != NULL) {
+      PropertyValue* property_value = page->GetProperty(
+          cohort, kCriticalImagesPropertyName);
+      // Read in the current critical images, and preserve the current HTML or
+      // CSS critical images if they are not being updated.
+      CriticalImages critical_images;
+      PopulateCriticalImagesFromPropertyValue(property_value, &critical_images);
+      bool updated = UpdateCriticalImages(
+          html_critical_images.get(), css_critical_images.get(),
+          &critical_images);
+      if (updated) {
+        GoogleString buf;
+        if (critical_images.SerializeToString(&buf)) {
+          // The property cache won't store an empty value, which is what an
+          // empty CriticalImages will serialize to. If buf is an empty string,
+          // repalce with a placeholder that we can then handle when decoding
+          // the property_cache value in
+          // PopulateCriticalImagesFromPropertyValue.
+          if (buf.empty()) {
+            buf = kEmptyValuePlaceholder;
+          }
+          page->UpdateValue(cohort, kCriticalImagesPropertyName, buf);
+        } else {
+          LOG(WARNING) << "Serialization of critical images protobuf failed.";
+          return false;
+        }
+      }
+      return updated;
+    } else {
+      LOG(WARNING) << "Critical Images Cohort is NULL.";
+    }
+  }
+  return false;
+}
+
+bool CriticalImagesFinder::UpdateCriticalImages(
+    const StringSet* html_critical_images,
+    const StringSet* css_critical_images,
+    CriticalImages* critical_images) const {
+  DCHECK(critical_images != NULL);
+  if (html_critical_images != NULL) {
+    UpdateCriticalImagesSetInProto(
+        *html_critical_images,
+        NumSetsToKeep(),
+        PercentSeenForCritical(),
+        critical_images->mutable_html_critical_images_sets(),
+        critical_images->mutable_html_critical_images());
+  }
+  if (css_critical_images != NULL) {
+    UpdateCriticalImagesSetInProto(
+        *css_critical_images,
+        NumSetsToKeep(),
+        PercentSeenForCritical(),
+        critical_images->mutable_css_critical_images_sets(),
+        critical_images->mutable_css_critical_images());
+  }
+  // We updated if either StringSet* was set.
+  return (html_critical_images != NULL || css_critical_images != NULL);
+}
+
+void CriticalImagesFinder::ExtractCriticalImagesFromCache(
+    RewriteDriver* driver,
+    const PropertyValue* property_value,
+    bool track_stats,
+    CriticalImagesInfo* critical_images_info) {
+  DCHECK(critical_images_info != NULL);
+  // Don't track stats if we are flushing early, since we will already be
+  // counting this when we are rewriting the full page.
+  track_stats &= !driver->flushing_early();
+  const PropertyCache* page_property_cache =
+      driver->server_context()->page_property_cache();
+  int64 cache_ttl_ms =
+      driver->options()->finder_properties_cache_expiration_time_ms();
+  // Check if the cache value exists and is not expired.
+  if (property_value->has_value()) {
+    const bool is_valid =
+        !page_property_cache->IsExpired(property_value, cache_ttl_ms);
+    if (is_valid) {
+      PopulateCriticalImagesInfoFromPropertyValue(property_value,
+                                                  critical_images_info);
+      if (track_stats) {
+        critical_images_valid_count_->Add(1);
+      }
+      return;
+    } else if (track_stats) {
+      critical_images_expired_count_->Add(1);
+    }
+  } else if (track_stats) {
+    critical_images_not_found_count_->Add(1);
+  }
+}
+
+}  // namespace net_instaweb
---
+++ tags/1.5.27.0/27/trunk/src/net/instaweb/rewriter/critical_selector_filter_test.cc
@@ -0,0 +1,310 @@
+/*
+ * Copyright 2013 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: morlovich@google.com (Maksim Orlovich)
+
+#include "net/instaweb/rewriter/public/critical_selector_filter.h"
+
+#include "net/instaweb/http/public/content_type.h"
+#include "net/instaweb/http/public/request_context.h"
+#include "net/instaweb/rewriter/public/critical_selector_finder.h"
+#include "net/instaweb/rewriter/public/rewrite_test_base.h"
+#include "net/instaweb/rewriter/public/rewrite_driver.h"
+#include "net/instaweb/rewriter/public/server_context.h"
+#include "net/instaweb/rewriter/public/test_rewrite_driver_factory.h"
+#include "net/instaweb/util/public/gtest.h"
+#include "net/instaweb/util/public/mock_property_page.h"
+#include "net/instaweb/util/public/property_cache.h"
+#include "net/instaweb/util/public/string.h"
+#include "net/instaweb/util/public/string_util.h"
+
+namespace net_instaweb {
+
+namespace {
+
+const char kRequestUrl[] = "http://www.example.com/";
+
+class CriticalSelectorFilterTest : public RewriteTestBase {
+ protected:
+  virtual void SetUp() {
+    RewriteTestBase::SetUp();
+    filter_ = new CriticalSelectorFilter(rewrite_driver());
+    rewrite_driver()->AppendOwnedPreRenderFilter(filter_);
+    server_context()->ComputeSignature(options());
+    server_context()->set_critical_selector_finder(
+        new CriticalSelectorFinder(RewriteDriver::kBeaconCohort, statistics()));
+
+    // Setup pcache.
+    pcache_ = rewrite_driver()->server_context()->page_property_cache();
+    SetupCohort(pcache_, RewriteDriver::kBeaconCohort);
+    SetupCohort(pcache_, RewriteDriver::kDomCohort);
+    ResetDriver();
+
+    // Write out some initial critical selectors for us to work with.
+    StringSet selectors;
+    selectors.insert("div");
+    selectors.insert("*");
+    server_context()->critical_selector_finder()->
+        WriteCriticalSelectorsToPropertyCache(selectors, rewrite_driver());
+    page_->WriteCohort(pcache_->GetCohort(RewriteDriver::kBeaconCohort));
+
+    // Some weird but valid CSS.
+    SetResponseWithDefaultHeaders("a.css", kContentTypeCss,
+                                  "div,span,*::first-letter { display: block; }"
+                                  "p { display: inline; }", 100);
+    SetResponseWithDefaultHeaders("b.css", kContentTypeCss,
+                                  "@media screen,print { * { margin: 0px; } }",
+                                  100);
+  }
+
+  void ResetDriver() {
+    rewrite_driver()->Clear();
+    rewrite_driver()->set_request_context(
+        RequestContext::NewTestRequestContext(factory()->thread_system()));
+    page_ = NewMockPage(kRequestUrl);
+    rewrite_driver()->set_property_page(page_);
+    pcache_->Read(page_);
+    SetHtmlMimetype();  // Don't wrap scripts in <![CDATA[ ]]>
+  }
+
+  GoogleString LoadRestOfCss(StringPiece orig_css) {
+    return StrCat("<noscript id=\"psa_add_styles\">", orig_css, "</noscript>",
+                  StrCat("<script type=\"text/javascript\">",
+                         CriticalSelectorFilter::kAddStylesScript,
+                         "</script>"));
+  }
+
+  GoogleString CssLinkHrefMedia(StringPiece url, StringPiece media) {
+    return StrCat("<link rel=stylesheet href=", url,
+                  " media=\"", media, "\">");
+  }
+
+  virtual bool AddHtmlTags() const { return false; }
+
+  CriticalSelectorFilter* filter_;  // owned by the driver;
+  PropertyCache* pcache_;
+  PropertyPage* page_;
+};
+
+TEST_F(CriticalSelectorFilterTest, BasicOperation) {
+  GoogleString css = StrCat(
+      "<style>*,p {display: none; } span {display: inline; }</style>",
+      CssLinkHref("a.css"),
+      CssLinkHref("b.css"));
+
+  GoogleString critical_css =
+      "*{display:none}"  // from the inline
+      "div,*::first-letter{display:block}"  // from a.css
+      "@media screen{*{margin:0px}}";  // from b.css
+
+  GoogleString html = StrCat(
+      "<head>",
+      css,
+      "</head>"
+      "<body><div>Stuff</div></body>");
+
+  // First run just collects the result into pcache
+  ValidateNoChanges("foo", html);
+
+  ResetDriver();
+  ValidateExpected(
+      "foo", html,
+      StrCat("<head><style>", critical_css, "</style></head>",
+             "<body><div>Stuff</div></body>",
+             LoadRestOfCss(css)));
+}
+
+TEST_F(CriticalSelectorFilterTest, Media) {
+  GoogleString css = StrCat(
+      "<style media=screen,print>*,p {display: none; } "
+          "span {display: inline; }</style>",
+      CssLinkHrefMedia("a.css", "screen"),
+      CssLinkHrefMedia("b.css", "screen and (color), aural"));
+
+  GoogleString critical_css =
+      "<style media=\"screen\">"
+          "*{display:none}"  // from the inline
+          "div,*::first-letter{display:block}"  // from a.css
+      "</style>"
+      "<style media=\"screen and (color)\">"
+          "@media screen{*{margin:0px}}"  // from b.css
+      "</style>";
+
+  GoogleString html = StrCat(
+      "<head>",
+      css,
+      "</head>"
+      "<body><div>Stuff</div></body>");
+
+  // First run just collects the result into pcache.
+  ValidateNoChanges("foo", html);
+
+  ResetDriver();
+  ValidateExpected(
+      "foo", html,
+      StrCat("<head>", critical_css, "</head>",
+             "<body><div>Stuff</div></body>",
+             LoadRestOfCss(css)));
+}
+
+TEST_F(CriticalSelectorFilterTest, NonScreenMedia) {
+  GoogleString css = StrCat(
+      "<style media=print>*,p {display: none; } "
+          "span {display: inline; }</style>",
+      CssLinkHrefMedia("a.css", "screen"),
+      CssLinkHrefMedia("b.css", "screen and (color), aural"));
+
+  GoogleString critical_css =
+      "<style media=\"screen\">"
+          "div,*::first-letter{display:block}"  // from a.css
+      "</style>"
+      "<style media=\"screen and (color)\">"
+          "@media screen{*{margin:0px}}"  // from b.css
+      "</style>";
+
+  GoogleString html = StrCat(
+      "<head>",
+      css,
+      "</head>"
+      "<body><div>Stuff</div></body>");
+
+  // First run just collects the result into pcache.
+  ValidateNoChanges("foo", html);
+
+  ResetDriver();
+  ValidateExpected(
+      "foo", html,
+      StrCat("<head>", critical_css, "</head>",
+             "<body><div>Stuff</div></body>",
+             LoadRestOfCss(css)));
+}
+
+TEST_F(CriticalSelectorFilterTest, SameCssDifferentSelectors) {
+  // We should not reuse results for same CSS when selectors are different.
+  GoogleString css = "<style>div,span { display: inline-block; }</style>";
+
+  GoogleString critical_css_div = "div{display:inline-block}";
+  GoogleString critical_css_span = "span{display:inline-block}";
+  GoogleString critical_css_div_span = "div,span{display:inline-block}";
+
+  // Check what we compute for a page with div.
+  ValidateNoChanges("with_div", StrCat(css, "<div>Foo</div>"));
+  ResetDriver();
+  ValidateExpected("with_div", StrCat(css, "<div>Foo</div>"),
+                   StrCat("<style>", critical_css_div, "</style>",
+                          "<div>Foo</div>", LoadRestOfCss(css)));
+
+  // Update the selector list with span. Because we are storing the last N
+  // beacon entries, both div and span should now be in the critical set. We
+  // also clear the property cache entry for our result, which is needed because
+  // the test harness is not really keying the pcache by the URL like the real
+  // system would.
+  StringSet selectors;
+  selectors.insert("span");
+  CriticalSelectorFinder* finder = server_context()->critical_selector_finder();
+  finder->WriteCriticalSelectorsToPropertyCache(selectors, rewrite_driver());
+  page_->WriteCohort(pcache_->GetCohort(RewriteDriver::kBeaconCohort));
+  page_->DeleteProperty(
+      pcache_->GetCohort(RewriteDriver::kDomCohort),
+      CriticalSelectorFilter::kSummarizedCssProperty);
+  page_->WriteCohort(pcache_->GetCohort(RewriteDriver::kDomCohort));
+
+  // Note that calling ResetDriver() just resets the state in the
+  // driver. Whatever has been written to the property cache so far
+  // will persist. Upon rewriting, the property cache contents will be read and
+  // the critical selector info in the driver will be repopulated.
+  ResetDriver();
+
+  ValidateNoChanges("with_div_span", StrCat(css, "<span>Foo</span>"));
+  ResetDriver();
+  ValidateExpected("with_div_span", StrCat(css, "<span>Foo</span>"),
+                   StrCat("<style>", critical_css_div_span, "</style>",
+                          "<span>Foo</span>", LoadRestOfCss(css)));
+
+  // Now send enough beacons to clear div from the critical selector set, so
+  // only span should be left.
+  for (int i = 0; i < finder->NumSetsToKeep(); ++i) {
+    finder->WriteCriticalSelectorsToPropertyCache(selectors, rewrite_driver());
+  }
+  page_->WriteCohort(pcache_->GetCohort(RewriteDriver::kBeaconCohort));
+  page_->DeleteProperty(
+      pcache_->GetCohort(RewriteDriver::kDomCohort),
+      CriticalSelectorFilter::kSummarizedCssProperty);
+  page_->WriteCohort(pcache_->GetCohort(RewriteDriver::kDomCohort));
+  ResetDriver();
+  ValidateNoChanges("with_span", StrCat(css, "<span>Foo</span>"));
+  ResetDriver();
+  ValidateExpected("with_span", StrCat(css, "<span>Foo</span>"),
+                   StrCat("<style>", critical_css_span, "</style>",
+                          "<span>Foo</span>", LoadRestOfCss(css)));
+}
+
+TEST_F(CriticalSelectorFilterTest, RetainPseudoOnly) {
+  // Make sure we handle things like :hover OK.
+  GoogleString css = ":hover { border: 2px solid red; }";
+  SetResponseWithDefaultHeaders("c.css", kContentTypeCss,
+                                css, 100);
+  ValidateNoChanges("hover", CssLinkHref("c.css"));
+  ResetDriver();
+  ValidateExpected("hover", CssLinkHref("c.css"),
+                   StrCat("<style>:hover{border:2px solid red}</style>",
+                          LoadRestOfCss(CssLinkHref("c.css"))));
+}
+
+TEST_F(CriticalSelectorFilterTest, RetainUnparseable) {
+  // Make sure we keep unparseable fragments around, particularly when
+  // the problem is with the selector, as well as with the entire region.
+  GoogleString css = "!huh! {background: white; } @huh { display: block; }";
+  SetResponseWithDefaultHeaders("c.css", kContentTypeCss,
+                                css, 100);
+  ValidateNoChanges("partly_unparseable", CssLinkHref("c.css"));
+  ResetDriver();
+  ValidateExpected(
+      "partly_unparseable", CssLinkHref("c.css"),
+      StrCat("<style>!huh! {background:#fff}@huh { display: block; }</style>",
+             LoadRestOfCss(CssLinkHref("c.css"))));
+}
+
+TEST_F(CriticalSelectorFilterTest, NoSelectorInfo) {
+  // Particular CSS doesn't matter here, just want some.
+  GoogleString css = "<style>div,span { display: inline-block; }</style>";
+
+  // We shouldn't change things when there is no info on selectors available.
+  page_->DeleteProperty(
+      pcache_->GetCohort(RewriteDriver::kBeaconCohort),
+      CriticalSelectorFinder::kCriticalSelectorsPropertyName);
+  page_->WriteCohort(pcache_->GetCohort(RewriteDriver::kBeaconCohort));
+
+  ResetDriver();
+  ValidateNoChanges("no_sel_info", StrCat(css, "<div>Foo</div>"));
+
+  ResetDriver();
+  ValidateNoChanges("no_sel_info", StrCat(css, "<div>Foo</div>"));
+}
+
+TEST_F(CriticalSelectorFilterTest, ResolveUrlsProperly) {
+  SetResponseWithDefaultHeaders("dir/c.css", kContentTypeCss,
+                                "* { background-image: url(d.png); }", 100);
+  ValidateNoChanges("rel_path", CssLinkHref("dir/c.css"));
+  ResetDriver();
+  ValidateExpected("rel_path", CssLinkHref("dir/c.css"),
+                   StrCat("<style>*{background-image:url(dir/d.png)}</style>",
+                          LoadRestOfCss(CssLinkHref("dir/c.css"))));
+}
+
+}  // namespace
+
+}  // namespace net_instaweb
