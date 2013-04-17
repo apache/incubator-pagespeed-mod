f5f8baf20a751072ac1c7634b7f200902566807f
matterbury@google.com <matterbury@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2013-04-17 19:52:49 +0000

    Create tag 1.5.27.1

---
+++ tags/1.5.27.1/src/net/instaweb/http/request_context.cc
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: piatek@google.com (Michael Piatek)
+
+#include "net/instaweb/http/public/request_context.h"
+
+#include "base/logging.h"
+#include "net/instaweb/http/public/log_record.h"
+#include "net/instaweb/util/public/abstract_mutex.h"
+#include "net/instaweb/util/public/request_trace.h"
+#include "net/instaweb/util/public/thread_system.h"
+
+namespace net_instaweb {
+
+RequestContext::RequestContext(AbstractMutex* logging_mutex)
+    : log_record_(new LogRecord(logging_mutex)),
+      using_spdy_(false) {
+}
+
+RequestContext::RequestContext() : using_spdy_(false) {}
+
+RequestContext::~RequestContext() {
+  // Please do not add non-diagnostic functionality here.
+  //
+  // RequestContexts are reference counted, and doing work in the dtor will
+  // result in actions being taken at unpredictable times, leading to difficult
+  // to diagnose performance and correctness bugs.
+}
+
+RequestContextPtr RequestContext::NewTestRequestContext(
+    ThreadSystem* thread_system) {
+  return RequestContextPtr(new RequestContext(thread_system->NewMutex()));
+}
+
+AbstractLogRecord* RequestContext::NewSubordinateLogRecord(
+    AbstractMutex* logging_mutex) {
+  return new LogRecord(logging_mutex);
+}
+
+void RequestContext::set_root_trace_context(RequestTrace* x) {
+  root_trace_context_.reset(x);
+}
+
+AbstractLogRecord* RequestContext::log_record() {
+  DCHECK(log_record_.get() != NULL);
+  return log_record_.get();
+}
+
+void RequestContext::WriteBackgroundRewriteLog() {
+  if (background_rewrite_log_record_.get() != NULL) {
+    background_rewrite_log_record_->WriteLog();
+  }
+}
+
+AbstractLogRecord* RequestContext::GetBackgroundRewriteLog(
+    ThreadSystem* thread_system,
+    bool log_urls,
+    bool log_url_indices,
+    int max_rewrite_info_log_size) {
+  // The mutex of the main log record is purposefully used to synchronize the
+  // creation of background log record.
+  ScopedMutex lock(log_record()->mutex());
+  AbstractLogRecord* log_record = background_rewrite_log_record_.get();
+  if (log_record == NULL) {
+    // We need to create a new log record.
+    log_record = NewSubordinateLogRecord(thread_system->NewMutex());
+    log_record->SetBackgroundRewriteInfo(log_urls, log_url_indices,
+         max_rewrite_info_log_size);
+    background_rewrite_log_record_.reset(log_record);
+  }
+  return log_record;
+}
+
+void RequestContext::ReleaseDependentTraceContext(RequestTrace* t) {
+  if (t != NULL) {
+    delete t;
+  }
+}
+
+}  // namespace net_instaweb
