1f6e1f7d2f3ff2b842cccb6135adf6dfad0e47db
sligocki@google.com <sligocki@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2013-05-28 17:16:29 +0000

    Create branch 28

---
+++ branches/28/src/net/instaweb/rewriter/delay_images.js
@@ -0,0 +1,167 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Code for loading high resolution critical images.
+ * This javascript is part of DelayImages filter.
+ *
+ * @author pulkitg@google.com (Pulkit Goyal)
+ */
+
+// Exporting functions using quoted attributes to prevent js compiler from
+// renaming them.
+// See http://code.google.com/closure/compiler/docs/api-tutorial3.html#dangers
+window['pagespeed'] = window['pagespeed'] || {};
+var pagespeed = window['pagespeed'];
+
+/**
+ * @constructor
+ */
+pagespeed.DelayImages = function() {
+  /*
+   * Boolean that controls whether the event handlers for lazy load are already
+   * registered.
+   * @type {boolean}
+   * @private
+   */
+  this.lazyLoadHighResHandlersRegistered_ = false;
+
+  /*
+   * Boolean that controls the logic to replace low res images with high res
+   * only once.
+   * @type {boolean}
+   * @private
+   */
+  this.highResReplaced_ = false;
+};
+
+/**
+ * For given elements, replace src with pagespeed_high_res_src if present.
+ * @param {NodeList.<Element>} elements list of DOM elements to check.
+ */
+pagespeed.DelayImages.prototype.replaceElementSrc = function(elements) {
+  for (var i = 0; i < elements.length; ++i) {
+    var src = elements[i].getAttribute('pagespeed_high_res_src');
+    if (src) {
+      elements[i].setAttribute('src', src);
+    }
+  }
+};
+pagespeed.DelayImages.prototype['replaceElementSrc'] =
+  pagespeed.DelayImages.prototype.replaceElementSrc;
+
+/**
+ * Runs the function when event is triggered.
+ * @param {Window|Element} elem Element to attach handler.
+ * @param {string} ev Name of the event.
+ * @param {function()} func New onload handler.
+ *
+ * TODO(nikhilmadan): Avoid duplication with the DeferJs code.
+ */
+pagespeed.addHandler = function(elem, ev, func) {
+  if (elem.addEventListener) {
+    elem.addEventListener(ev, func, false);
+  } else if (elem.attachEvent) {
+    elem.attachEvent('on' + ev, func);
+  } else {
+    var oldHandler = elem['on' + ev];
+    elem['on' + ev] = function() {
+      func.call(this);
+      if (oldHandler) {
+        oldHandler.call(this);
+      }
+    };
+  }
+};
+
+/**
+ * Register the event handlers to lazy load the high res images. This is
+ * called only when lazyload_high_res_experimental flag is enabled.
+ */
+pagespeed.DelayImages.prototype.registerLazyLoadHighRes = function() {
+  // Add event handlers only once
+  if (this.lazyLoadHighResHandlersRegistered_) {
+    this.highResReplaced_ = false;
+    return;
+  }
+
+  var elem = document.body;
+  var interval = 500;
+  var tapStart, tapEnd = 0;
+
+  var me = this;
+
+  this.highResReplaced = false;
+  if ('ontouchstart' in elem) {
+    pagespeed.addHandler(elem, 'touchstart', function(e) {
+      tapStart = Date.now();
+    });
+
+    pagespeed.addHandler(elem, 'touchend', function(e) {
+      tapEnd = Date.now();
+      // Load the high res images if there is a multi-touch or if the tap
+      // duration is less than 500ms i.e single click. The timer catches the
+      // click event sooner than the click handler on most phones.
+      if ((e.changedTouches != null && e.changedTouches.length == 2) ||
+          (e.touches != null && e.touches.length == 2) ||
+          tapEnd - tapStart < interval) {
+        me.loadHighRes();
+      }
+    });
+  } else {
+    pagespeed.addHandler(window, 'click', function(e) {
+      me.loadHighRes();
+    });
+  }
+  pagespeed.addHandler(window, 'load', function(e) {
+    me.loadHighRes();
+  });
+  this.lazyLoadHighResHandlersRegistered_ = true;
+};
+
+pagespeed.DelayImages.prototype['registerLazyLoadHighRes'] =
+  pagespeed.DelayImages.prototype.registerLazyLoadHighRes;
+
+/**
+ * Triggered from event handlers that were previously registered. Replaces
+ * low res images with high res sources.
+ */
+pagespeed.DelayImages.prototype.loadHighRes = function() {
+  if (!this.highResReplaced_) {
+    this.replaceWithHighRes();
+    this.highResReplaced_ = true;
+  }
+};
+
+/**
+ * Replaces low resolution image with high resolution image.
+ */
+pagespeed.DelayImages.prototype.replaceWithHighRes = function() {
+  this.replaceElementSrc(document.getElementsByTagName('img'));
+  this.replaceElementSrc(document.getElementsByTagName('input'));
+};
+pagespeed.DelayImages.prototype['replaceWithHighRes'] =
+  pagespeed.DelayImages.prototype.replaceWithHighRes;
+
+/**
+ * Initializes the delay images module.
+ */
+pagespeed.delayImagesInit = function() {
+  var temp = new pagespeed.DelayImages();
+  pagespeed['delayImages'] = temp;
+};
+
+pagespeed['delayImagesInit'] = pagespeed.delayImagesInit;
---
+++ branches/28/src/pagespeed/kernel/image/jpeg_reader.cc
@@ -0,0 +1,273 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: Bryan McQuade, Matthew Steele
+
+#include "pagespeed/kernel/image/jpeg_reader.h"
+
+#include <setjmp.h>
+#include <stdlib.h>
+
+#include "pagespeed/kernel/base/string.h"
+
+extern "C" {
+#ifdef USE_SYSTEM_LIBJPEG
+#include "jerror.h"                                                 // NOLINT
+#include "jpeglib.h"                                                // NOLINT
+#else
+#include "third_party/libjpeg/jerror.h"
+#include "third_party/libjpeg/jpeglib.h"
+#endif
+}
+
+namespace {
+
+// Unfortunately, libjpeg normally only supports reading images from C FILE
+// pointers, wheras we want to read from a C++ string.  Fortunately, libjpeg
+// also provides an extension mechanism.  Below, we define a new kind of
+// jpeg_source_mgr for reading from strings.
+
+// The below code was adapted from the JPEGMemoryReader class that can be found
+// in src/o3d/core/cross/bitmap_jpg.cc in the Chromium source tree (r29423).
+// That code is Copyright 2009, Google Inc.
+
+METHODDEF(void) InitSource(j_decompress_ptr cinfo) {}
+
+METHODDEF(boolean) FillInputBuffer(j_decompress_ptr cinfo) {
+  // Should not be called because we already have all the data
+  ERREXIT(cinfo, JERR_INPUT_EOF);
+  return TRUE;
+}
+
+METHODDEF(void) SkipInputData(j_decompress_ptr cinfo,
+                              long num_bytes) {              // NOLINT
+  jpeg_source_mgr &mgr = *(cinfo->src);
+  const int bytes_remaining = mgr.bytes_in_buffer - num_bytes;
+  mgr.bytes_in_buffer = bytes_remaining < 0 ? 0 : bytes_remaining;
+  mgr.next_input_byte += num_bytes;
+}
+
+METHODDEF(void) TermSource(j_decompress_ptr cinfo) {}
+
+// Call this function on a j_decompress_ptr to install a reader that will read
+// from the given string.
+void JpegStringReader(j_decompress_ptr cinfo,
+                      const void* image_data,
+                      size_t image_length) {
+  if (cinfo->src == NULL) {
+    cinfo->src = (struct jpeg_source_mgr*)
+      (*cinfo->mem->alloc_small) ((j_common_ptr) cinfo, JPOOL_PERMANENT,
+                                  sizeof(jpeg_source_mgr));
+  }
+  struct jpeg_source_mgr &src = *(cinfo->src);
+
+  src.init_source = InitSource;
+  src.fill_input_buffer = FillInputBuffer;
+  src.skip_input_data = SkipInputData;
+  src.resync_to_restart = jpeg_resync_to_restart;  // default method
+  src.term_source = TermSource;
+
+  src.bytes_in_buffer = image_length;
+  src.next_input_byte = static_cast<const JOCTET*>(image_data);
+}
+
+// ErrorExit() is installed as a callback, called on errors
+// encountered within libjpeg.  The longjmp jumps back
+// to the setjmp in JpegOptimizer::CreateOptimizedJpeg().
+void ErrorExit(j_common_ptr jpeg_state_struct) {
+  jmp_buf *env = static_cast<jmp_buf *>(jpeg_state_struct->client_data);
+  (*jpeg_state_struct->err->output_message)(jpeg_state_struct);
+  if (env)
+    longjmp(*env, 1);
+}
+
+// OutputMessageFromReader is called by libjpeg code on an error when reading.
+// Without this function, a default function would print to standard error.
+void OutputMessage(j_common_ptr jpeg_decompress) {
+  // The following code is handy for debugging.
+  /*
+  char buf[JMSG_LENGTH_MAX];
+  (*jpeg_decompress->err->format_message)(jpeg_decompress, buf);
+  DLOG(INFO) << "JPEG Reader Error: " << buf;
+  */
+}
+
+}  // namespace
+
+namespace pagespeed {
+
+namespace image_compression {
+
+struct JpegEnv {
+  jpeg_decompress_struct jpeg_decompress_;
+  jpeg_error_mgr decompress_error_;
+  jmp_buf jmp_buf_env_;
+};
+
+JpegReader::JpegReader() {
+  jpeg_decompress_ = static_cast<jpeg_decompress_struct*>(
+      malloc(sizeof(jpeg_decompress_struct)));
+  decompress_error_ = static_cast<jpeg_error_mgr*>(
+      malloc(sizeof(jpeg_error_mgr)));
+  memset(jpeg_decompress_, 0, sizeof(jpeg_decompress_struct));
+  memset(decompress_error_, 0, sizeof(jpeg_error_mgr));
+
+  jpeg_decompress_->err = jpeg_std_error(decompress_error_);
+  decompress_error_->error_exit = &ErrorExit;
+  decompress_error_->output_message = &OutputMessage;
+  jpeg_create_decompress(jpeg_decompress_);
+}
+
+JpegReader::~JpegReader() {
+  jpeg_destroy_decompress(jpeg_decompress_);
+  free(decompress_error_);
+  free(jpeg_decompress_);
+}
+
+void JpegReader::PrepareForRead(const void* image_data, size_t image_length) {
+  // Prepare to read from a string.
+  JpegStringReader(jpeg_decompress_, image_data, image_length);
+}
+
+JpegScanlineReader::JpegScanlineReader() :
+  jpeg_env_(NULL),
+  pixel_format_(UNSUPPORTED),
+  height_(0),
+  width_(0),
+  row_(0),
+  bytes_per_row_(0),
+  was_initialized_(false) {
+  row_pointer_[0] = NULL;
+}
+
+JpegScanlineReader::~JpegScanlineReader() {
+  if (was_initialized_) {
+    Reset();
+  }
+  free(jpeg_env_);
+}
+
+bool JpegScanlineReader::Reset() {
+  pixel_format_ = UNSUPPORTED;
+  height_ = 0;
+  width_ = 0;
+  row_ = 0;
+  bytes_per_row_ = 0;
+  was_initialized_ = false;
+
+  jpeg_destroy_decompress(&(jpeg_env_->jpeg_decompress_));
+  memset(jpeg_env_, 0, sizeof(JpegEnv));
+  free(row_pointer_[0]);
+  row_pointer_[0] = NULL;
+  return true;
+}
+
+bool JpegScanlineReader::Initialize(const void* image_data,
+                                    size_t image_length) {
+  if (was_initialized_) {
+    // Reset the reader if it has been initialized before.
+    Reset();
+  } else if (jpeg_env_ == NULL) {
+    jpeg_env_ = static_cast<JpegEnv*>(malloc(sizeof(JpegEnv)));
+    memset(jpeg_env_, 0, sizeof(JpegEnv));
+  }
+
+  // libjpeg's error handling mechanism requires that longjmp be used
+  // to get control after an error.
+  if (setjmp(jpeg_env_->jmp_buf_env_)) {
+    // This code is run only when libjpeg hit an error and called
+    // longjmp(env). It will reset the object to a state where it can be used
+    // again.
+    Reset();
+    return false;
+  }
+
+  jpeg_error_mgr* decompress_error = &(jpeg_env_->decompress_error_);
+  jpeg_decompress_struct* jpeg_decompress = &(jpeg_env_->jpeg_decompress_);
+  jpeg_decompress->err = jpeg_std_error(decompress_error);
+  decompress_error->error_exit = &ErrorExit;
+  decompress_error->output_message = &OutputMessage;
+  jpeg_create_decompress(jpeg_decompress);
+
+  // Need to install env so that it will be longjmp()ed to on error.
+  jpeg_decompress->client_data = static_cast<void *>(jpeg_env_->jmp_buf_env_);
+
+  // Prepare to read from a string.
+  JpegStringReader(jpeg_decompress, image_data, image_length);
+
+  // Read jpeg data into the decompression struct.
+  jpeg_read_header(jpeg_decompress, TRUE);
+
+  width_ = jpeg_decompress->image_width;
+  height_ = jpeg_decompress->image_height;
+
+  // Decode the image to GRAY_8 if it was in gray scale, or to RGB_888
+  // otherwise.
+  if (jpeg_decompress->jpeg_color_space == JCS_GRAYSCALE) {
+    jpeg_decompress->out_color_space = JCS_GRAYSCALE;
+    pixel_format_ = GRAY_8;
+    bytes_per_row_ = width_;
+  } else {
+    jpeg_decompress->out_color_space = JCS_RGB;
+    pixel_format_ = RGB_888;
+    bytes_per_row_ = 3 * width_;
+  }
+
+  was_initialized_ = true;
+  return true;
+}
+
+bool JpegScanlineReader::ReadNextScanline(void** out_scanline_bytes) {
+  if (!was_initialized_ || !HasMoreScanLines()) {
+    return false;
+  }
+
+  if (setjmp(jpeg_env_->jmp_buf_env_)) {
+    // This code is run only when libjpeg hit an error and called
+    // longjmp(env). It will reset the object to a state where it can be used
+    // again.
+    Reset();
+    return false;
+  }
+
+  // At the time when ReadNextScanline is called, allocate buffer for holding
+  // a row of pixels, and initiate decompression.
+  jpeg_decompress_struct* jpeg_decompress = &(jpeg_env_->jpeg_decompress_);
+  if (row_ == 0) {
+    row_pointer_[0] = static_cast<JSAMPLE*>(malloc(bytes_per_row_));
+    jpeg_start_decompress(jpeg_decompress);
+  }
+
+  // Try to read a scanline.
+  const JDIMENSION num_scanlines_read =
+      jpeg_read_scanlines(jpeg_decompress, row_pointer_, 1);
+  if (num_scanlines_read != 1) {
+    Reset();
+    return false;
+  }
+  *out_scanline_bytes = row_pointer_[0];
+  ++row_;
+
+  // At the last row, ask libjpeg to finish decompression.
+  if (!HasMoreScanLines()) {
+    jpeg_finish_decompress(jpeg_decompress);
+  }
+  return true;
+}
+
+}  // namespace image_compression
+
+}  // namespace pagespeed
