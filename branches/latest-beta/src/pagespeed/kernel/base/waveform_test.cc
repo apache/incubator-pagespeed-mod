5e6c308312a946955f35776ad1499c2f871e263e
jmaessen@google.com <jmaessen@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2014-04-23 22:00:12 +0000

    Mark patched beta as latest.

---
+++ branches/latest-beta/src/pagespeed/kernel/base/waveform_test.cc
@@ -0,0 +1,188 @@
+// Copyright 2011 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// Author: jmarantz@google.com (Joshua Marantz)
+
+#include "pagespeed/kernel/base/waveform.h"
+
+#include "pagespeed/kernel/base/gtest.h"
+#include "pagespeed/kernel/base/mock_message_handler.h"
+#include "pagespeed/kernel/base/mock_timer.h"
+#include "pagespeed/kernel/base/scoped_ptr.h"
+#include "pagespeed/kernel/base/statistics.h"
+#include "pagespeed/kernel/base/string.h"
+#include "pagespeed/kernel/base/string_util.h"
+#include "pagespeed/kernel/base/string_writer.h"
+#include "pagespeed/kernel/base/thread_system.h"
+#include "pagespeed/kernel/util/platform.h"
+#include "pagespeed/kernel/util/simple_stats.h"
+
+namespace {
+
+const char kCoordinateFormat[] = "[%f, %f]";
+
+}  // namespace
+
+namespace net_instaweb {
+
+class WaveformTest : public testing::Test {
+ protected:
+  WaveformTest()
+      : thread_system_(Platform::CreateThreadSystem()),
+        timer_(thread_system_->NewMutex(), MockTimer::kApr_5_2010_ms),
+        stats_(thread_system_.get()),
+        handler_(thread_system_->NewMutex()) {
+  }
+
+  GoogleString Format(int time_ms, int value) {
+    return StringPrintf(kCoordinateFormat, 1.0 * time_ms, 1.0 * value);
+  }
+
+  bool Contains(const StringPiece& html, int time_ms, int value) {
+    return (html.find(Format(time_ms, value)) != GoogleString::npos);
+  }
+
+  scoped_ptr<ThreadSystem> thread_system_;
+  MockTimer timer_;
+  SimpleStats stats_;
+  MockMessageHandler handler_;
+};
+
+
+// A basic sanity test showing that the header loads the jsapi.
+TEST_F(WaveformTest, Header) {
+  GoogleString html;
+  StringWriter writer(&html);
+  Waveform::RenderHeader(&writer, &handler_);
+  EXPECT_NE(GoogleString::npos, html.find("www.google.com/jsapi"));
+}
+
+// Instantiate a waveform and make sure one of the values shows up.
+TEST_F(WaveformTest, BasicGraph) {
+  Variable* variable = stats_.AddVariable("test1");
+
+  Waveform waveform(thread_system_.get(), &timer_, 10, variable);
+  timer_.SetTimeUs(MockTimer::kApr_5_2010_ms);
+  waveform.Add(10);
+  timer_.AdvanceMs(10);
+  waveform.Add(20);
+  timer_.AdvanceMs(10);
+  waveform.Add(10);
+  timer_.AdvanceMs(10);
+  waveform.Add(30);
+  timer_.AdvanceMs(10);
+  waveform.Add(10);
+  timer_.AdvanceMs(10);
+  waveform.Add(40);
+  timer_.AdvanceMs(10);
+  waveform.Add(10);
+  timer_.AdvanceMs(10);
+  waveform.Add(50);
+  timer_.AdvanceMs(10);
+  waveform.Add(10);
+  timer_.AdvanceMs(10);
+  waveform.Add(60);
+  timer_.AdvanceMs(10);
+
+  GoogleString html;
+  StringWriter writer(&html);
+  waveform.Render("My Waveform", "My Values", &writer, &handler_);
+  EXPECT_TRUE(Contains(html, 90, 60));
+  EXPECT_NE(GoogleString::npos, html.find("'My Waveform'"));
+  EXPECT_NE(GoogleString::npos, html.find("'My Values'"));
+  // The last Add call is sticky.
+  EXPECT_EQ(60, variable->Get());
+}
+
+// Instantiate a waveform and make sure one of the values shows up.
+TEST_F(WaveformTest, Delta) {
+  Variable* variable = stats_.AddVariable("test1");
+  Waveform waveform(thread_system_.get(), &timer_, 10, variable);
+  timer_.SetTimeUs(MockTimer::kApr_5_2010_ms);
+  waveform.AddDelta(10);
+  timer_.AdvanceMs(10);
+  waveform.AddDelta(20);
+  timer_.AdvanceMs(10);
+  waveform.AddDelta(-5);
+  timer_.AdvanceMs(10);
+
+  GoogleString html;
+  StringWriter writer(&html);
+  waveform.Render("My Waveform", "My Values", &writer, &handler_);
+  EXPECT_TRUE(Contains(html, 20, 25));
+  EXPECT_NE(GoogleString::npos, html.find("'My Waveform'"));
+  EXPECT_NE(GoogleString::npos, html.find("'My Values'"));
+  EXPECT_EQ(10 + 20 - 5, variable->Get());
+}
+
+// Overflows the number of samples and makes sure the desired results
+// are shown.
+TEST_F(WaveformTest, Overflow) {
+  Waveform waveform(thread_system_.get(), &timer_, 10, NULL /* variable */);
+
+  // Don't overflow at first.
+  for (int i = 0; i < 10; ++i) {
+    waveform.Add(i);
+    timer_.AdvanceMs(10);
+  }
+  GoogleString html;
+  StringWriter writer(&html);
+
+  waveform.Render("My Waveform", "My Values", &writer, &handler_);
+  EXPECT_TRUE(Contains(html, 0, 0));
+  EXPECT_TRUE(Contains(html, 10, 1));
+  EXPECT_TRUE(Contains(html, 80, 8));
+  EXPECT_TRUE(Contains(html, 90, 9));
+
+  // Now overflow the first 2 entries and re-render.  The first &
+  // second values (0,0) and (10,1) should be gone, but note that
+  // the time-axis is all deltas so the x-values always start at 0.
+  for (int i = 10; i < 12; ++i) {  // knocks off first 2
+    waveform.Add(i);
+    timer_.AdvanceMs(10);
+  }
+
+  html.clear();
+  waveform.Render("My Waveform", "My Values", &writer, &handler_);
+  EXPECT_FALSE(Contains(html, 0, 1));   // truncated
+  EXPECT_FALSE(Contains(html, 10, 1));  // truncated
+  EXPECT_TRUE(Contains(html, 0, 2));
+  EXPECT_TRUE(Contains(html, 80, 10));
+  EXPECT_TRUE(Contains(html, 90, 11));
+
+  // The rest of the values should be present in the HTML and in order.
+  GoogleString::size_type prev_pos = 0;
+  for (int i = 0; i < 10; ++i) {
+    GoogleString::size_type pos = html.find(Format(10 * i, i + 2));
+    ASSERT_NE(GoogleString::npos, pos);
+    EXPECT_LT(prev_pos, pos);
+    prev_pos = pos;
+  }
+}
+
+TEST_F(WaveformTest, AvgMinMax) {
+  Waveform waveform(thread_system_.get(), &timer_, 10, NULL /* variable */);
+  for (int i = 1; i <= 1000; ++i) {
+    waveform.Add(i);
+    timer_.AdvanceMs(10);
+  }
+
+  // Note that the first value involved in the average is 0 due to the
+  // fact that we are accumulating delta_time*value quantities.
+  EXPECT_EQ(500.0, waveform.Average());
+  EXPECT_EQ(1.0, waveform.Minimum());
+  EXPECT_EQ(1000.0, waveform.Maximum());
+}
+
+}  // namespace net_instaweb
---
+++ branches/latest-beta/src/pagespeed/kernel/cache/mock_time_cache_test.cc
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// Author: morlovich@google.com (Maksim Orlovich)
+
+// Unit-test for MockTimeCache
+
+#include "pagespeed/kernel/cache/mock_time_cache.h"
+
+#include <cstddef>
+
+#include "pagespeed/kernel/base/basictypes.h"
+#include "pagespeed/kernel/base/gtest.h"
+#include "pagespeed/kernel/base/mock_timer.h"
+#include "pagespeed/kernel/base/scoped_ptr.h"
+#include "pagespeed/kernel/base/shared_string.h"
+#include "pagespeed/kernel/base/thread_system.h"
+#include "pagespeed/kernel/cache/cache_test_base.h"
+#include "pagespeed/kernel/cache/lru_cache.h"
+#include "pagespeed/kernel/thread/mock_scheduler.h"
+#include "pagespeed/kernel/util/platform.h"
+
+namespace net_instaweb {
+namespace {
+
+const size_t kMaxSize = 100;
+
+// We start time from a non-zero value to make sure we don't confuse
+// relative and absolute. The value itself is of no particular significance.
+const int64 kStartTime = 3456;
+
+class MockTimeCacheTest : public CacheTestBase {
+ protected:
+  MockTimeCacheTest()
+      : thread_system_(Platform::CreateThreadSystem()),
+        timer_(thread_system_->NewMutex(), kStartTime),
+        scheduler_(thread_system_.get(), &timer_),
+        lru_cache_(kMaxSize),
+        cache_(&scheduler_, &lru_cache_) {
+  }
+
+  virtual CacheInterface* Cache() { return &cache_; }
+
+  void AdvanceTimeUs(int64 interval_us) {
+    scheduler_.AdvanceTimeUs(interval_us);
+  }
+
+ protected:
+  scoped_ptr<ThreadSystem> thread_system_;
+  MockTimer timer_;
+  MockScheduler scheduler_;
+  LRUCache lru_cache_;
+  MockTimeCache cache_;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MockTimeCacheTest);
+};
+
+TEST_F(MockTimeCacheTest, NoDelayOps) {
+  // Basic operation w/o any delay injected.
+  CheckNotFound("Name");
+  CheckPut("Name", "Value");
+  CheckGet("Name", "Value");
+  cache_.Delete("Name");
+  CheckNotFound("Name");
+}
+
+TEST_F(MockTimeCacheTest, DelayOps) {
+  const int64 kDelayUs = 10000;
+  cache_.set_delay_us(kDelayUs);
+
+  // Load the value.
+  CheckPut("Name", "Value");
+
+  // Try getting...
+  CacheTestBase::Callback result;
+  cache_.Get("Name", &result);
+
+  // Initially, should not have been called.
+  EXPECT_FALSE(result.called());
+
+  // Move halfways to completion; should still have not been called.
+  AdvanceTimeUs(kDelayUs / 2);
+  EXPECT_FALSE(result.called());
+
+  // Now after it expires, it should be OK.
+  AdvanceTimeUs(kDelayUs / 2 + 1);
+  EXPECT_TRUE(result.called());
+  EXPECT_EQ(CacheInterface::kAvailable, result.state());
+  EXPECT_EQ("Value", result.value()->Value());
+
+  // Do the same thing after deleting it.
+  cache_.Delete("Name");
+  cache_.Get("Name", result.Reset());
+  EXPECT_FALSE(result.called());
+  AdvanceTimeUs(kDelayUs / 2);
+  EXPECT_FALSE(result.called());
+  AdvanceTimeUs(kDelayUs / 2 + 1);
+  EXPECT_TRUE(result.called());
+  EXPECT_EQ(CacheInterface::kNotFound, result.state());
+}
+
+}  // namespace
+}  // namespace net_instaweb
