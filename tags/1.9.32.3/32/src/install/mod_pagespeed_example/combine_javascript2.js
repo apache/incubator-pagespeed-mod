d18a9d76d007e5ca674e1bb1a425c2c476a23d5a
jcrowell@google.com <jcrowell@google.com@b28baae1-b17d-8ef2-060a-10057fd0e0fa>
2014-12-11 16:05:44 +0000

    remove tag for retagging

--- tags/1.9.32.3/32/src/install/mod_pagespeed_example/combine_javascript2.js
+++
@@ -1,4 +0,0 @@
-window.onload = function() {
-  logText("Application started.");
-  logText("Library operation confirmed.");
-}
--- tags/1.9.32.3/32/src/net/instaweb/rewriter/testdata/extScript.js
+++
@@ -1,3 +0,0 @@
-/* Inject state into document */
-document.write("External " + state);
-state += 1;  // Then update it.
