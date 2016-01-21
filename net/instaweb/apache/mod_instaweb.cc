3e66c7353d72797a37b565116eed98499c030da2
Jeff Kaufman <jefftk@google.com>
2016-01-21 16:45:40 -0500

    Move InstawebHandler initialization after validity checks.
    Creating an instaweb handler will run MakeRequestUrl, which assumes that
    request->unparsed_uri is non-null.  So move the creation to after where
    we check that it's non-null.  To be safe, move it all the way down to
    where it's first needed, in case some other validity checks end up being
    relevant.
    
    Fixes #1248

--- net/instaweb/apache/mod_instaweb.cc
+++ net/instaweb/apache/mod_instaweb.cc
@@ -371,10 +371,6 @@ InstawebContext* build_context_for_request(request_rec* request) {
     return NULL;
   }
 
-  InstawebHandler instaweb_handler(request);
-  const RewriteOptions* options = instaweb_handler.options();
-  instaweb_handler.SetupSpdyConnectionIfNeeded();
-
   if (request->unparsed_uri == NULL) {
     // TODO(jmarantz): consider adding Debug message if unparsed_uri is NULL,
     // possibly of request->the_request which was non-null in the case where
@@ -438,6 +434,10 @@ InstawebContext* build_context_for_request(request_rec* request) {
                   "Request not rewritten because: X-Mod-Pagespeed header set.");
     return NULL;
   }
+
+  InstawebHandler instaweb_handler(request);
+  const RewriteOptions* options = instaweb_handler.options();
+  instaweb_handler.SetupSpdyConnectionIfNeeded();
 
   const GoogleUrl& stripped_gurl = instaweb_handler.stripped_gurl();
   if (!stripped_gurl.IsWebValid()) {
--- net/instaweb/apache/system_test.sh
+++ net/instaweb/apache/system_test.sh
@@ -2672,6 +2672,11 @@ start_test Base config has purging disabled.  Check error message syntax.
 OUT=$($WGET_DUMP "$HOSTNAME/pagespeed_admin/cache?purge=*")
 check_from "$OUT" fgrep -q "ModPagespeedEnableCachePurge on"
 
+start_test long url handling
+# This is an extremely long url, enough that it should give a 4xx server error.
+OUT=$($CURL -sS -D- "$TEST_ROOT/$(head -c 10000 < /dev/zero | tr '\0' 'a')")
+check_from "$OUT" grep -q "414 Request-URI Too Large"
+
 # Cleanup
 rm -rf $OUTDIR
 
