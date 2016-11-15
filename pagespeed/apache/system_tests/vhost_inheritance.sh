6a882b2d5f6884c6450ef163978057689cbd6098
Steve Hill <cheesy@google.com>
2016-11-15 18:03:59 -0500

    Fix vhost_inheritance.sh

--- pagespeed/apache/system_tests/vhost_inheritance.sh
+++ pagespeed/apache/system_tests/vhost_inheritance.sh
@@ -1,3 +1,5 @@
+config_title="<title>PageSpeed Configuration</title>"
+
 if [ "$NO_VHOST_MERGE" = "on" ]; then
   start_test Config with VHost inheritance off
   echo $WGET_DUMP $SECONDARY_CONFIG_URL
@@ -12,7 +14,6 @@ else
   start_test vhost inheritance works
   echo $WGET_DUMP $SECONDARY_CONFIG_URL
   SECONDARY_CONFIG=$($WGET_DUMP $SECONDARY_CONFIG_URL)
-  config_title="<title>PageSpeed Configuration</title>"
   check_from "$SECONDARY_CONFIG" fgrep -q "$config_title"
   # Sharding is applied in this host, thanks to global inherit flag.
   check_from "$SECONDARY_CONFIG" egrep -q "http://nonspdy.example.com/"
