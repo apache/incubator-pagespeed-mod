92080c45959a90bcb2fa570c4f3a49b7e85e97bb
Otto van der Schaaf <oschaaf@we-amp.com>
2018-01-15 20:23:40 +0000

    Fixup net/instaweb/js/data_to_c.cc

--- net/instaweb/js/data_to_c.cc
+++ net/instaweb/js/data_to_c.cc
@@ -1,3 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 #include <algorithm>
 #include <cstdlib>
@@ -15,21 +33,6 @@ DEFINE_string(c_file, "/tmp/a.c", "Output C file");
 DEFINE_string(varname, "str", "Variable name.");
 
 const char kOutputTemplate[] =
-    "// Copyright 2011 Google Inc. All Rights Reserved.\n"
-    "//\n"
-    "// Licensed under the Apache License, Version 2.0 (the \"License\");\n"
-    "// you may not use this file except in compliance with the License.\n"
-    "// You may obtain a copy of the License at\n"
-    "//\n"
-    "//      http://www.apache.org/licenses/LICENSE-2.0\n"
-    "//\n"
-    "// Unless required by applicable law or agreed to in writing, software\n"
-    "// distributed under the License is distributed on an \"AS IS\" BASIS,\n"
-    "// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied"
-    ".\n"
-    "// See the License for the specific language governing permissions and\n"
-    "// limitations under the License.\n"
-    "\n"
     "// Automatically generated from %s\n"
     "\n"
     "namespace net_instaweb {\n"
