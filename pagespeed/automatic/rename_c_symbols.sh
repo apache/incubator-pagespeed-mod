c8e76915f672e2153eff33a7f83112f32ffb2429
Otto van der Schaaf <oschaaf@we-amp.com>
2018-01-18 21:52:30 +0000

    Fix double shebangs

--- pagespeed/automatic/rename_c_symbols.sh
+++ pagespeed/automatic/rename_c_symbols.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-#!/bin/sh
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- pagespeed/system/remote_config_test.sh
+++ pagespeed/system/remote_config_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#!/bin/bash
 #
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
