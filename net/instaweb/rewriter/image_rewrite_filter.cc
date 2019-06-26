773ef4005ff0cf84b9fe6705a8d9ee4ecdd64f26
Lofesa <lofesa@gmail.com>
2019-06-26 13:46:50 +0200

    Update image_rewrite_filter.cc

--- net/instaweb/rewriter/image_rewrite_filter.cc
+++ net/instaweb/rewriter/image_rewrite_filter.cc
@@ -2138,12 +2138,14 @@ void ImageRewriteFilter::StartElementImpl(HtmlElement* element) {
     BeginRewriteImageUrl(element, attributes[i].url);
    
   
-    HtmlElement::Attribute* srcset[] = {element->FindAttribute(HtmlName::kSrcset),element->FindAttribute(HtmlName::kDataSrcset)};
-    for (int i = 0; i == 1; i++){
-       if (srcset[i] != nullptr){
-	       BeginRewriteSrcSet(element, srcset[i]);
-        }
-    }
+    HtmlElement::Attribute* srcset = element->FindAttribute(HtmlName::kSrcset);
+    HtmlElement::Attribute* datasrcset = element->FindAttribute(HtmlName::kDataSrcset);
+    if (srcset != nullptr) {
+	    BeginRewriteSrcSet(element, srcset);
+    } else if (datasrcset != nullptr) {
+	    BeginRewriteSrcSet(element, datasrcset);
+    }	    
+  
   }
 }
 
