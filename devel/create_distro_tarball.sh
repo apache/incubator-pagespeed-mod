dd6e061bb986094a9189f100e384540122f8639b
Otto van der Schaaf <oschaaf@we-amp.com>
2019-09-19 00:02:30 +0200

    Address release rc review feedback
    - Don't use src/ in the tarball we produce
    - Update LICENSE & NOTICE
    
    Signed-off-by: Otto van der Schaaf <oschaaf@we-amp.com>

--- devel/create_distro_tarball.sh
+++ devel/create_distro_tarball.sh
@@ -61,7 +61,7 @@ function config {
 #
 # Also, BUILDTYPE=Release can be passed to make (the default is Debug).
 echo "Generating src/Makefile"
-src/build/gyp_chromium -D use_system_libs=1 \$*
+build/gyp_chromium --depth=. -D use_system_libs=1 \$*
 SCRIPT_END
 }
 
@@ -96,8 +96,8 @@ DIR="$WRAPPER_DIR/src"
 ln -s "$MPS_CHECKOUT" "$TEMP_DIR/$DIR"
 
 # Also create a little helper script that shows how to run gyp
-config > "$TEMP_DIR/$WRAPPER_DIR/generate.sh"
-chmod +x "$TEMP_DIR/$WRAPPER_DIR/generate.sh"
+config > "$TEMP_DIR/$DIR/generate.sh"
+chmod +x "$TEMP_DIR/$DIR/generate.sh"
 
 # Normally, the build system runs build/lastchange.sh to figure out what
 # to put into the last portion of the version number. We are, however, going to
@@ -108,28 +108,28 @@ chmod +x "$TEMP_DIR/$WRAPPER_DIR/generate.sh"
 
 # Things that depends on minimal or not.
 if [ $MINIMAL -eq 0 ]; then
-  GTEST=$DIR/testing
-  GFLAGS=$DIR/third_party/gflags
-  GIFLIB=$DIR/third_party/giflib
-  ICU="$DIR/third_party/icu/icu.gyp \
-      $DIR/third_party/icu/source/common/unicode/"
-  JSONCPP=$DIR/third_party/jsoncpp
-  LIBWEBP=$DIR/third_party/libwebp
-  PROTOBUF=$DIR/third_party/protobuf
-  RE2=$DIR/third_party/re2
+  GTEST=testing
+  GFLAGS=third_party/gflags
+  GIFLIB=third_party/giflib
+  ICU="third_party/icu/icu.gyp \
+      third_party/icu/source/common/unicode/"
+  JSONCPP=third_party/jsoncpp
+  LIBWEBP=third_party/libwebp
+  PROTOBUF=third_party/protobuf
+  RE2=third_party/re2
 else
-  GTEST="$DIR/testing \
-      --exclude $DIR/testing/gmock \
-      --exclude $DIR/testing/gtest"
-  GFLAGS=$DIR/third_party/gflags/gflags.gyp
-  GIFLIB=$DIR/third_party/giflib/giflib.gyp
-  ICU=$DIR/third_party/icu/icu.gyp
-  JSONCPP=$DIR/third_party/jsoncpp/jsoncpp.gyp
-  LIBWEBP="$DIR/third_party/libwebp/COPYING \
-      $DIR/third_party/libwebp/examples/gif2webp_util.*"
-  PROTOBUF="$DIR/third_party/protobuf/*.gyp \
-      $DIR/third_party/protobuf/*.gypi"
-  RE2=$DIR/third_party/re2/re2.gyp
+  GTEST="testing \
+      --exclude testing/gmock \
+      --exclude testing/gtest"
+  GFLAGS=third_party/gflags/gflags.gyp
+  GIFLIB=third_party/giflib/giflib.gyp
+  ICU=third_party/icu/icu.gyp
+  JSONCPP=third_party/jsoncpp/jsoncpp.gyp
+  LIBWEBP="third_party/libwebp/COPYING \
+      third_party/libwebp/examples/gif2webp_util.*"
+  PROTOBUF="third_party/protobuf/*.gyp \
+      third_party/protobuf/*.gypi"
+  RE2=third_party/re2/re2.gyp
 fi
 
 # It's tarball time!
@@ -139,60 +139,64 @@ fi
 cd "$TEMP_DIR"
 tar cj --dereference --exclude='.git' --exclude='.svn' --exclude='.hg' -f $TARBALL \
     --exclude='*.mk' --exclude='*.pyc' \
-    --exclude=$DIR/net/instaweb/genfiles/*/*.cc \
-    $WRAPPER_DIR/generate.sh \
-    $DIR/DISCLAIMER \
-    $DIR/LICENSE \
-    $DIR/NOTICE \
-    $DIR/README.md \
-    $DIR/LASTCHANGE.in \
-    $DIR/base \
-    $DIR/build \
-    --exclude $DIR/build/android/arm-linux-androideabi-gold \
-    $DIR/install \
-    $DIR/net/instaweb \
-    $DIR/pagespeed \
-    $DIR/strings \
+    --exclude=third_party/chromium/src/build/android/* \
+    --exclude=net/instaweb/genfiles/*/*.cc \
+    -C $DIR \
+    generate.sh \
+    DISCLAIMER \
+    LICENSE \
+    NOTICE \
+    README.md \
+    LASTCHANGE.in \
+    base \
+    build \
+    --exclude build/android/arm-linux-androideabi-gold \
+    install \
+    net/instaweb \
+    pagespeed \
+    strings \
     $GTEST \
-    $DIR/third_party/apr/apr.gyp \
-    $DIR/third_party/aprutil/aprutil.gyp \
-    $DIR/third_party/aprutil/apr_memcache2.h \
-    $DIR/third_party/aprutil/apr_memcache2.c \
-    $DIR/third_party/httpd/httpd.gyp \
-    $DIR/third_party/httpd24/httpd24.gyp \
-    $DIR/third_party/base64 \
-    $DIR/third_party/brotli \
-    $DIR/third_party/chromium/src/base \
+    third_party/apr/apr.gyp \
+    third_party/aprutil/aprutil.gyp \
+    third_party/aprutil/apr_memcache2.h \
+    third_party/aprutil/apr_memcache2.c \
+    third_party/httpd/httpd.gyp \
+    third_party/httpd24/httpd24.gyp \
+    third_party/base64 \
+    third_party/brotli \
+    third_party/chromium/src/base \
     --exclude src/third_party/chromium/src/base/third_party/xdg_mime \
     --exclude src/third_party/chromium/src/base/third_party/xdg_user_dirs \
-    $DIR/third_party/chromium/src/build \
-    --exclude $DIR/third_party/chromium/src/build/android \
-    $DIR/third_party/chromium/src/googleurl \
-    $DIR/third_party/chromium/src/net/tools \
-    $DIR/third_party/closure/ \
-    $DIR/third_party/closure_library/ \
-    $DIR/third_party/css_parser \
-    $DIR/third_party/domain_registry_provider \
+    third_party/chromium/src/build \
+    --exclude third_party/chromium/src/build/android \
+    third_party/chromium/src/googleurl \
+    third_party/chromium/src/net/tools \
+    third_party/closure/ \
+    third_party/closure_library/ \
+    third_party/css_parser \
+    third_party/domain_registry_provider \
     $GFLAGS \
     $GIFLIB \
-    $DIR/third_party/google-sparsehash \
-    $DIR/third_party/grpc \
-    $DIR/third_party/hiredis \
+    third_party/google-sparsehash \
+    third_party/grpc \
+    third_party/hiredis \
     $ICU \
     $JSONCPP \
-    $DIR/third_party/libjpeg_turbo/libjpeg_turbo.gyp \
-    $DIR/third_party/libpng/libpng.gyp \
+    third_party/libjpeg_turbo/libjpeg_turbo.gyp \
+    third_party/libpng/libpng.gyp \
     $LIBWEBP \
-    $DIR/third_party/modp_b64 \
-    $DIR/third_party/optipng \
+    third_party/modp_b64 \
+    third_party/optipng \
     $PROTOBUF  \
-    $DIR/third_party/rdestl \
+    third_party/rdestl \
     $RE2 \
-    $DIR/third_party/redis-crc \
-    $DIR/third_party/serf \
-    $DIR/third_party/zlib/zlib.gyp \
-    $DIR/tools/gyp \
-    $DIR/url
+    third_party/redis-crc \
+    third_party/serf \
+    third_party/zlib/zlib.gyp \
+    tools/gyp \
+    url
+
+echo $TARBALL
 
 cd "$MPS_CHECKOUT"
 rm -r "$TEMP_DIR"
